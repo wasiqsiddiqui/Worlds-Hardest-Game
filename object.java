e57bac99ce56c574ba69205d5a1b15634b3cdc73
Wasiq Siddiqui <wasiqsiddiqui@yahoo.com>
2016-08-11 12:02:30 -0500

    Update object.java

--- object.java
+++ object.java
@@ -6,96 +6,46 @@ import javax.swing.Timer;
 import static java.lang.System.*;
 
 public class object extends JPanel {
-
 	public Rectangle character;
-
 	public int charW = 24;
-
 	public int charH= 36;
-
 	public boolean right = false;
-
 	public boolean left = false;
-
+	
 public object(){
-
 	character = new Rectangle(180, 180, charW, charH);
-
 	addKeyListener(new KeyAdapter() {
-
-		public void keyPressed(KeyEvent e){
-
-			if(e.getKeyCode() == KeyEvent.VK_D){
-
-				right = true;
-
-				character.x += 1;
-
-						}
-
-			if(e.getKeyCode() == KeyEvent.VK_A){
-
-					left= true;
-
-					character.x -= 1;
-
-						}
-
-
-
-			}
-
-			public void keyReleased(KeyEvent e){
-
-				if(e.getKeyCode() == KeyEvent.VK_D){
-
-					right = false;
-
-							}
-
-				if(e.getKeyCode() == KeyEvent.VK_A){
-					left = false;
-
-						}
-
-
-
-
-
-}
-
-public void paintComponent(Graphics g){
-
-super.paintComponent(g);
-
-g.setColor(Color.white);
-
-g.fillRect(character.x, character.y, character.width, character.height);
-
-if(right){
-
-	character.x +=1;
-
-		}
-
-if(left){
-
-	character.x -=1;
-
-		}
-
-	repaint();
-
-
-									}
-
-
-
+public void keyPressed(KeyEvent e){
+	if(e.getKeyCode() == KeyEvent.VK_D){
+		right = true;
+		character.x += 1;
 	}
-
+	if(e.getKeyCode() == KeyEvent.VK_A){
+		left= true;
+		character.x -= 1;
+		}
+	}
+public void keyReleased(KeyEvent e){
+	if(e.getKeyCode() == KeyEvent.VK_D){
+		right = false;
+	}
+	if(e.getKeyCode() == KeyEvent.VK_A){
+		left = false;
+	}
+}
+public void paintComponent(Graphics g){
+super.paintComponent(g);
+g.setColor(Color.white);
+g.fillRect(character.x, character.y, character.width, character.height);
+	if(right){
+	character.x +=1;
+		}
+if(left){
+	character.x -=1;
+		}
+	repaint();
+	}
+     }
 }
 
 }
-
-
-
