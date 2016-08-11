3da1f1d4f44046ca473cf4f9d99058a8decf4dd1
Wasiq Siddiqui <wasiqsiddiqui@yahoo.com>
2016-08-11 11:58:52 -0500

    Update floorframe.java

--- floorframe.java
+++ floorframe.java
@@ -6,18 +6,12 @@ import javax.swing.Timer;
 import static java.lang.System.*;
 
 public class floorframe extends JFrame {
-
 {
 player theplayer;
-
 theplayer = new player();
-
 getContentPane().add(theplayer);
-
 theplayer.setFocusable(true);
-
 theplayer.requestFocusInWindow();
-
 new Timer(1-1000, theplayer).start();
 }
 
